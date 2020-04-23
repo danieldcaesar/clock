27eed709c20dc298a9f9c6880c273401e67e3bca
danieldcaesar <danieldcaesar@gmail.com>
2020-04-22 21:38:35 -0400

    final
    added cubic bezier transition to hands
    fixed hour hand

--- src.js
+++ src.js
@@ -4,8 +4,18 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setTime() {
     const now = new Date();
-    const seconds = now.getSeconds();
 
+    const seconds = now.getSeconds();
+    const secDegrees = (seconds/60)*360 + 90;
+    secondHand.style.transform = `rotate(${secDegrees}deg)`;
     console.log(seconds);
+
+    const minutes = now.getMinutes();
+    const minDegrees = (minutes/60)*360 + 90;
+    minuteHand.style.transform = `rotate(${minDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hrsDegrees = (hours/12)*360 + 90;
+    hourHand.style.transform = `rotate(${hrsDegrees}deg)`;
 }
 setInterval(setTime, 1000);
